0183bda918bf04bbdb313a9dcac227a15fbe33b3
danya2271 <danya2271@yandex.ru>
2023-03-17 20:23:13 +0500

    Overclock cpu up to 1.75 GHz

--- gcc.sh
+++ gcc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make CROSS_COMPILE=/mnt/alps/Download/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-elf/bin/aarch64-elf- O=out ARCH=arm64 -j4 $1 $2 $3 $4 $5 
+make CROSS_COMPILE=/run/media/danya227/19060ec9-c8fb-4059-b47a-54b93f1fcb86/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-elf/bin/aarch64-elf- O=out ARCH=arm64 -j4 $1 $2 $3 $4 $5
