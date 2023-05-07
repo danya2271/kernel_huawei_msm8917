aa6ba2f4d509c6b5f28cd802c9e202a4b9fa0ae9
danya227 <danya2271@yandex.ru>
2023-05-07 09:53:26 +0500

    Add script for downloading gcc

---
+++ prepare_compiler.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+tar -xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+rm -rf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
